1de9d055792da29d806ad8dbdd80b02170c70a5f
Sebastian Quilitz <zeraphim@x-pantion.de>
2025-04-18 12:58:58 +0000

    add copyright header to each source file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-//! This example shows how to use UART (Universal asynchronous receiver-transmitter) in the RP2040 chip.
-//!
-//! No specific hardware is specified in this example. Only output on pin 0 is tested.
-//! The Raspberry Pi Debug Probe (https://www.raspberrypi.com/products/debug-probe/) could be used
-//! with its UART port.
+/* RP2350 GameBoy cartridge
+ * Copyright (C) 2025 Sebastian Quilitz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #![no_std]
 #![no_main]
